02b5ccc1cfdf0c99502013e135982e1080e11770
DeotusW <deotus_wilson@trimble.com>
2025-09-26 10:28:19 +0530

    26- sept

--- MainFolder/StepName_and_StepNumber/Testcases/test_Negativecases.py
+++ MainFolder/StepName_and_StepNumber/Testcases/test_Negativecases.py
@@ -1,41 +1,48 @@
 import json
 from pathlib import Path
+import pytest
 from BaseFolder.Baseclass import Baseclass
 from RequestMethods.requestMethods import RequestMain
-from StepName_and_StepNumber.Testcases.filetest import CurrentPath
 
+__testdata = None
+
+# Loading Testdata from Json file
+
+def get_testdata():
+    global __testdata
+    if __testdata is None:
+        CurrentPath = Path(__file__)
+        try:
+            with open(CurrentPath.parent.parent / "Testdata" / "stepNumber_and_stepNumber_Testdata.json", "r") as f:
+                __testdata = json.load(f)
+        except FileNotFoundError:
+            raise FileNotFoundError("The Testdata JSON file is missing - Not Found!")
+    return __testdata
 
 class TestNegativeCases(Baseclass):
 
-    URL = "https://hardware-activation.stg.agco-fuse-services.com/v1alpha1/statuses/"
+    URL = "https://hrgav1lgvd.execute-api.eu-west-1.amazonaws.com/v1alpha1/statuses/"
     STATUS_CODE = 400
     CurrentFilePath = Path(__file__).resolve()
-    testdata = None
 
-    #Loading Testdata from Json file
-    @property
-    def loadtestdata(self):
-        if self.testdata is None:
-            with open(CurrentPath.parent.parent/"Testdata"/"stepNumber_and_stepNumber_Testdata.json", "r") as f:
-                self.testdata = json.load(f)
-        return self.testdata
+    @pytest.mark.parametrize("tcs", get_testdata())
+    def test_negative_case(self,tcs):
+            try:
+                self.url = self.URL
+                self.param = {
+                    "currentStepName": tcs["currentStepName"],
+                    "currentStepNumber": tcs["currentStepNumber"]
+                }
+                self.headers = {
+                    "Authorization": f"Bearer {self.token}"
+                }
+                response = RequestMain().get_method(self.url,params=self.param,headers=self.headers)
+                jsonresponse = response.json()
+                RequestMain().validate_statuscode(response, self.STATUS_CODE)
+                assert jsonresponse["message"] == tcs["ExpectedMessage"], "Expected Message doesn't Match!"
 
-
-    def test_negative_case(self):
-
-        for tcs in self.loadtestdata:
-            self.url = self.URL
-            self.param = {
-                "currentStepName": tcs["currentStepName"],
-                "currentStepNumber": tcs["currentStepNumber"]
-            }
-            self.headers = {
-                "Authorization": f"Bearer {self.token}"
-            }
-            response = RequestMain().get_method(self.url,params=self.param,headers=self.headers)
-            jsonresponse = response.json()
-            RequestMain().validate_statuscode(response, self.STATUS_CODE)
-            assert jsonresponse["message"] == tcs["ExpectedMessage"], "Expected Message doesn't Match!"
+            except Exception as e:
+                raise AssertionError(f"Testcase Failed: {e}")
 
 
 
