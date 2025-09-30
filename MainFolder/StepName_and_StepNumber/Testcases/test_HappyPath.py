e92161152a594bfb13ed105b57ca4fd3fef3c278
DeotusW <deotus_wilson@trimble.com>
2025-09-30 15:20:15 +0530

    Update test_HappyPath.py

--- MainFolder/StepName_and_StepNumber/Testcases/test_HappyPath.py
+++ MainFolder/StepName_and_StepNumber/Testcases/test_HappyPath.py
@@ -7,7 +7,7 @@ class TestHappyPath(Baseclass):
 
     expected = {
     "serialNumber": "0412-000105",
-    "currentStepName": "Activation_Send_NCP",
+    "currentStepName": "Activation_Send_NC",
     "currentStepNumber": 6
     }
 
@@ -42,5 +42,6 @@ class TestHappyPath(Baseclass):
             print(f" ID: {jsonresponse[0]['id']}")
         except Exception as e:
             raise AssertionError(f"Testcase Failed: {e}")
+
 
 
